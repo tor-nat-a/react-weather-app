aeb637295e89798d623fc2c23d43ca467a2d4677
tor-nat-a <nattorhon@gmail.com>
2022-08-30 18:22:19 +0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather
+      <h1>Weather</h1>
+      <footer>
+      This project was coded by Nataliia Torhonska and is 
+      <a href="https://github.com/tor-nat-a/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
