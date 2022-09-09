93540db268df8b82b8da773b7063c1fffadc4168
tor-nat-a <nattorhon@gmail.com>
2022-09-09 12:25:01 +0300

    Added weather for 5 days

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import "./App.css";
-import React from 'react';
+import React from "react";
 import Weather from "./Weather";
-const App = () => {
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather</h1>
-        <Weather defaultCity="Kyiv" />
+        <Weather defaultCity="New York" />
+
         <footer>
-          This project was coded by Nataliia Torhonska and is
+          This project was coded by Nataliia Torhonska
+          and is{" "}
           <a
             href="https://github.com/tor-nat-a/react-weather-app"
-            rel="noreferrer"
+            target="_blank" rel="noreferrer"
           >
-            {" "}
             open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://benevolent-buttercream-0f4bf2.netlify.app/"
+            target="_blank" rel="noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
     </div>
   );
-};
-
-export default App;
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,29 +1,27 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
   }
 
-  function search() {
-    const apiKey = "56b34e8362b10a3f785244b7f4f660a7";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-  }
   function handleSubmit(event) {
     event.preventDefault();
     search();
@@ -31,6 +29,12 @@ export default function Weather(props) {
 
   function handleCityChange(event) {
     setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   if (weatherData.ready) {
@@ -41,7 +45,7 @@ export default function Weather(props) {
             <div className="col-9">
               <input
                 type="search"
-                placeholder="Enter a city"
+                placeholder="Enter a city.."
                 className="form-control"
                 autoFocus="on"
                 onChange={handleCityChange}
@@ -57,7 +61,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
